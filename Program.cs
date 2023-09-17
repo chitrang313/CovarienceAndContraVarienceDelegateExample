27f5dc1236038e35f4568eddc8468fb584bd221d
chitrang <chitrang313@gmail.com>
2023-09-17 11:10:58 +0530

    log car details using delegates

--- Program.cs
+++ Program.cs
@@ -1,23 +1,27 @@
 ﻿using System.Reflection;
 
 class Program {
-    private delegate Car CarFactoryDel(int id, string name);    
+    private delegate Car CarFactoryDel(int id, string name);
+
+    private delegate void LogIceCarDetail(ICECar iceCar);
+    private delegate void LogEVCarDetail(EVCar evCar);
 
     static void Main(string[] args) {
         CarFactoryDel carDel = CarFactory.ReturnICECar;
 
         Car iceCar = carDel(1, "Audi A8");
-
-        Console.WriteLine($"{iceCar.GetType()}");
-        Console.WriteLine($"{iceCar.GetCarDetails()}");
-       
+        
+        LogIceCarDetail iceCarLodDel = CarFactory.LogCarDetails;        
+        iceCarLodDel(iceCar as ICECar);
+        
         carDel = CarFactory.ReturnEVCar;
 
         Car evCar = carDel(2,"Tesla S9");
 
-        Console.WriteLine();
-        Console.WriteLine(evCar.GetType());
-        Console.WriteLine(evCar.GetCarDetails());
+        LogEVCarDetail evCarLogDetail = CarFactory.LogCarDetails;
+        evCarLogDetail(evCar as EVCar);
+
+        Console.ReadLine();
     }
 }
 
@@ -28,6 +32,19 @@ public static class CarFactory {
 
     public static EVCar ReturnEVCar(int id,string name) {
         return new EVCar { Id = id,Name = name };
+    }
+
+    public static void LogCarDetails(Car car) {
+        if(car is ICECar) {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ICECarLogFile.txt");
+            using (StreamWriter sw = new StreamWriter(filePath,true)) {
+                sw.WriteLine(car.GetCarDetails());
+            }
+        }else if (car is EVCar) {
+            Console.WriteLine(car.GetCarDetails());
+        }else {
+            throw new ArgumentException();
+        }
     }
 }
 
